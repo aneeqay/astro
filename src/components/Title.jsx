5d1f57c5300765821a0e4cdc9a6fe857bcf31a5c
Aneeqa Yusaf <aneeqayusaf@outlook.com>
2023-10-28 13:32:24 +0100

    added cs and confetti

---
+++ src/components/Title.jsx
@@ -0,0 +1,7 @@
+const Title = () => {
+    return ( 
+        <h1 className="text-base font-semibold leading-7 text-indigo-600">Hello World!</h1>
+     );
+}
+ 
+export default Title;
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,6 +2,12 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		colors: {
+			'fuchsia': '#FB4570',
+			'hot-pink': '#FB6B90',
+			'pink': '#FB8DA0',
+			'off-white': '#EFEBE0',
+		},
 		extend: {},
 	},
 	plugins: [],
